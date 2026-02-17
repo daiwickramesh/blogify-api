e1bac87417bc72978d0257a347035a0a029241f5
daiwickramesh <daiwickramesh1999@gmail.com>
2026-02-17 12:55:57 +0530

    feat: modularize post routes using express.Router

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+const postRouter = require('./src/routes/posts.routes.js');
+
+// Home route
+app.get('/', (req, res) => {
+  res.send('Welcome to Blogify API');
+});
+
+// Mount router
+app.use('/api/v1/posts', postRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+}); 
